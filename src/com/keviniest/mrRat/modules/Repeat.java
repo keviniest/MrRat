475e6a6ed45a4bfd33b92fdc73754904aec4d896
Keviniest <kevinistmc@gmail.com>
2021-12-17 16:47:52 +0900

    fixed repeat

--- src/com/keviniest/mrRat/modules/Repeat.java
+++ src/com/keviniest/mrRat/modules/Repeat.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class Repeat extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        String[] args = event.getMessage().getContentRaw().split(" {2}");
+        String[] args = event.getMessage().getContentRaw().split(":");
         if(args[0].equalsIgnoreCase(MrRat.prefix + "repeat")) {
             event.getChannel().sendMessage(args[1]).queue();
             System.out.println(event.getAuthor() + " : " + args[1]);
